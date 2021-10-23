7e6c2512f8d9c422982222538d638856473360b2
Szymon Kasprzycki <66119938+Szymon-Kasprzycki@users.noreply.github.com>
2021-10-23 13:56:45 +0200

    Added uppercase letters support
    You can choose if you want also uppercase letters in your password.

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ import string
 import random
 
 
-def generate(length: int = 16, allowedChars: str = None):
+def generate(length: int = 16, allowedChars: str = None, UpperCase: bool = False):
     alphabet = string.ascii_lowercase + string.digits
     if allowedChars:
         alphabet += allowedChars
+    if UpperCase:
+        alphabet += string.ascii_uppercase
     password = "".join(random.choices(alphabet, k=length))
     return password
 
@@ -21,20 +23,44 @@ def save(filename: str, password: str):
     return
 
 
-if __name__ == "__main__":
+def main():
     len = input(
         "Tell me the length of the password (leave blank if don't know) --> "
     ).strip()
     allowedChars = input(
         "Tell me the special characters you want to be used in password eg. *&$@#?. --> "
     ).strip()
+    upperCase = (
+        input("Do you want uppercase letters in your password? (y/n) --> ")
+        .strip()
+        .lower()
+    )
+    if upperCase == "y":
+        upperCase = True
+    elif upperCase == "n":
+        upperCase = False
+    else:
+        upperCase = None
+        print("Please type in correct values in uppercase letters question!")
+        return
+
     if len:
         try:
             len = int(len)
-        except Exception as e:
+        except Exception:
             print("This length is not allowed!")
-    passwd = generate(len, allowedChars)
-    save("passwd", passwd)
+            return
+    passwd = generate(len, allowedChars, upperCase)
+    try:
+        save("passwd", passwd)
+    except PermissionError:
+        print("Couldn't save file with password. Don't have enough permissions!")
+    except Exception as e:
+        print(f"Error: {e}")
     print(
         "Your password was successfully generated, you can find it in 'passwd.txt' file in script's root directory."
     )
+
+
+if __name__ == "__main__":
+    main()
